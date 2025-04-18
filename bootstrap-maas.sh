5367ed32d105a5ed97a13aa14fee7b5fdca751f2
Rick Doucet <rick@richard-doucet.com>
2025-04-18 10:37:49 -0600

    Update bootstrap-maas.sh
    escape \$ to make it work

--- bootstrap-maas.sh
+++ bootstrap-maas.sh
@@ -93,12 +93,10 @@ server {
         proxy_pass http://localhost:$MAAS_PORT/MAAS/;
         proxy_http_version 1.1;
 
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-
-        # 🔥 Required for WebSockets to work
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
     }
 }
